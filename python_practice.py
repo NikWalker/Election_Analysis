cfde34f74feb81ae995d1d2a6a0a4f5e6ce9bb6a
Nik Walker <ketchupkid17@gmail.com>
2022-11-25 12:15:17 -0500

    add files

--- python_practice.py
+++ python_practice.py
@@ -4,4 +4,17 @@ if counties[1]=='Denver':
 if 'El Paso' in counties:
     print("El Paso is in the list of counties.")
 else:
-    print("El Paso is not in the list of counties.")    
+    print("El Paso is not in the list of counties.")
+if 'Arapahoe' in  counties and 'El Paso'not in counties:
+    print("only Arapahoe and El Paso are in the list of counties")
+else:
+    print('Arapahoe or El Paso is not in the list of counties') 
+for county in counties:
+    print(county)      
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}   
+for county in counties_dict.keys():
+    print (county)
+for voters in counties_dict.values():
+    print (voters)
+for county, voters in counties_dict.items():
+    print(county,'county has', voters,'registered voters.')
